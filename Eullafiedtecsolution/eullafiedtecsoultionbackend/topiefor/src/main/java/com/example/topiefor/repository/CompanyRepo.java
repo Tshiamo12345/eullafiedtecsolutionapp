4c1a4bb1c95e098b692ad6fffec835b696959950
Tshiamo12345 <apollosejake@gmail.com>
2025-04-06 10:10:35 +0100

    Modified the Company class

--- Eullafiedtecsolution/eullafiedtecsoultionbackend/topiefor/src/main/java/com/example/topiefor/repository/CompanyRepo.java
+++ Eullafiedtecsolution/eullafiedtecsoultionbackend/topiefor/src/main/java/com/example/topiefor/repository/CompanyRepo.java
@@ -2,10 +2,18 @@ package com.example.topiefor.repository;
 
 import com.example.topiefor.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CompanyRepo extends JpaRepository<Company,String> {
 
 
+    Optional<Company> findByName(String name);
+    Optional<Company> findBySlogan(String name);
+    @Query ("SELECT c FROM company c JOIN FETCH c.addresses a JOIN FETCH a.location WHERE c.companyId = :companyId")
+    Optional<Company> findByIdWithAddressesAndLocation(@Param("companyId") String companyId);
 }
