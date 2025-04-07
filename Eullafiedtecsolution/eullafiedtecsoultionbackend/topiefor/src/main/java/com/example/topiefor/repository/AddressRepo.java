55490c915ef96e98a088734409f16d86cffc1266
Tshiamo12345 <apollosejake@gmail.com>
2025-04-07 10:57:06 +0100

    modified

---
+++ Eullafiedtecsolution/eullafiedtecsoultionbackend/topiefor/src/main/java/com/example/topiefor/repository/AddressRepo.java
@@ -0,0 +1,15 @@
+package com.example.topiefor.repository;
+
+import com.example.topiefor.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepo extends JpaRepository<Address, String> {
+
+
+
+
+}
